f20404143df938156b2d1fd41c3fdddcf16b7b83
mlkc084 <mlkc084@gmail.com>
2023-01-24 20:42:57 -0800

    Fixed code to run in Python 3 instead of Python 2.

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ for x in range(5):
 
 def print_board(board):
     for row in board:
-        print
-        " ".join(row)
+        print(" ".join(row))
 
 
 print_board(board)
@@ -31,31 +30,25 @@ ship_col = random_col(board)
 # Everything from here on should go in your for loop!
 # Be sure to indent four spaces!
 for turn in range(4):
-    guess_row = int(raw_input("Guess Row: "))
-    guess_col = int(raw_input("Guess Col: "))
+    guess_row = int(input("Guess Row: "))
+    guess_col = int(input("Guess Col: "))
 
     if guess_row == ship_row and guess_col == ship_col:
-        print
-        "Congratulations! You sunk my battleship!"
+        print("Congratulations! You sunk my battleship!")
         break
     else:
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print
-            "Oops, that's not even in the ocean."
+            print("Oops, that's not even in the ocean.")
         elif (board[guess_row][guess_col] == "X"):
-            print
-            "You guessed that one already."
+            print("You guessed that one already.")
         else:
-            print
-            "You missed my battleship!"
+            print("You missed my battleship!")
             board[guess_row][guess_col] = "X"
         # Print (turn + 1) here!
-        print
-        "Turn", turn + 1
+        print("Turn", turn + 1)
         print_board(board)
         if turn == 3:
-            print
-            "Game Over"
+            print("Game Over")
 
 
 
